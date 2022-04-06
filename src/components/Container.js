d7a3317a97c74e97ce839259d5387c41629f008e
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2022-04-06 12:56:50 +0600

    Base css style

---
+++ src/components/Container.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import "./record.scss";
+
+function Container() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default Container;
